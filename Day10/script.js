d5e684600503809f276ac4cb0794da01accb9388
ParagNukte <paragnuktek@gmail.com>
2024-08-05 23:16:49 +0530

    Day 10 finish

--- Day10/script.js
+++ Day10/script.js
@@ -19,17 +19,17 @@ const mouseoverEvent = document.getElementById("task1");
 mouseoverEvent.addEventListener("mouseover", function () {
   mouseoverEvent.style.backgroundColor = "green";
 });
-
+//Task 4: Add a mouseout event listener and reset its background color
 const task1Element = document.getElementById("task1");
 task1Element.addEventListener("mouseout", function () {
   task1Element.style.backgroundColor = "white";
 });
-
+//Task 5: Add a keydown event listener to an input field that displays key pressed on the console
 document.getElementById("task5").addEventListener("keydown", function (event) {
   let word = event.target.value;
   console.log(word);
 });
-
+//Task 6: Add a keyup event listener to an input field that displays the current value in the paragraph
 document.getElementById("task5").addEventListener("keyup", function (event) {
   let word = event.target.value;
   if (event.key === "ArrowUp") {
@@ -38,15 +38,40 @@ document.getElementById("task5").addEventListener("keyup", function (event) {
     document.getElementById("input-display").innerHTML = null;
   }
 });
-
+//Task 7: Add a submit event  to a form that prevents the default submission and log the form data to the console.
 const form = document.getElementById("myform");
 form.addEventListener("submit", function (event) {
-  
   event.preventDefault();
-  let name = document.getElementById("formname")
-  let number = document.getElementById("formnumber")
+  let name = document.getElementById("formname");
+  let number = document.getElementById("formnumber");
   console.log(`name: ${name.value}, number: ${number.value}`);
-  name.value= "";
+  name.value = "";
   number.value = "";
+});
+//Task 8: Add a change event listener on a dropdown and display the selected option on the paragraph
+const dropDisplay = document.getElementById("drop-display");
+const display = document.getElementById("display-drop");
+dropDisplay.addEventListener("change", function () {
+  const displayCountry = dropDisplay.value;
+  display.innerHTML = displayCountry;
+});
 
+//Event Delegation:
+// Task 9: Add a event listener to a list that logs the text content of the clicked list item using event delgation
+
+const listItem = document.getElementById("list-items");
+listItem.addEventListener("click", function (event) {
+  console.log(event.target.innerText);
+});
+
+// Task 10: Add a event listener to a parent element that listens for the events from dynamically addded child elements
+
+const parentDiv = document.querySelector('#parent-div');
+
+// Add an event listener to the parent element
+parentDiv.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        // Handle the event for dynamically added buttons
+        console.log('Button clicked:', event.target.innerText);
+    }
 });
